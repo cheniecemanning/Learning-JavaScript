58871906be2094df0517551fd289d5171cd4a77c
Cheniece Manning <cheniecemanning@hotmail.com>
2018-03-12 17:17:00 +0000

    Added console logs to game

--- blackjack.js
+++ blackjack.js
@@ -6,4 +6,8 @@ Author: Cheniece Manning
 let card1 = "Ace of Spades",
     card2 = "Ten of Hearts";
 
-console.log("Welcome to Blackjack!")
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
