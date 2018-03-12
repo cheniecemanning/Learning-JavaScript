db65b2512bd5cfee4c7dc268cdf4f6f60e0eb068
Cheniece Manning <cheniecemanning@hotmail.com>
2018-03-12 18:01:26 +0000

    Added suits and value arrays

--- blackjack.js
+++ blackjack.js
@@ -3,14 +3,27 @@ Blackjack
 Author: Cheniece Manning
 */
 
-let deck = ["Ace of Spades", "Two of Spades", "Three of Spades"];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
+let deck = [];
 let playerCards = [deck[0], deck[2]];
-let card1 = "Ace of Spades",
-    card2 = "Ten of Hearts";
+
+for (let suitIdx = 0; suitIdx<suits.length; suitIdx++){
+ 
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++){
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
+
+for (let i = 0; i < deck.length; i++){
+    console.log(deck[i]);
+}
 
 console.log("Welcome to Blackjack");
 
 console.log("You are dealt: ");
 console.log(" " + playerCards[0]);
 console.log(" " + playerCards[1]);
+
+
 
