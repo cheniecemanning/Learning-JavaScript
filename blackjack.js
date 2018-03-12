5c122b8c1c563dbc14a02ecfb20900d484feb774
Cheniece Manning <cheniecemanning@hotmail.com>
2018-03-12 17:21:28 +0000

    Added Deck and Player card arrays

--- blackjack.js
+++ blackjack.js
@@ -3,11 +3,14 @@ Blackjack
 Author: Cheniece Manning
 */
 
+let deck = ["Ace of Spades", "Two of Spades", "Three of Spades"];
+let playerCards = [deck[0], deck[2]];
 let card1 = "Ace of Spades",
     card2 = "Ten of Hearts";
 
 console.log("Welcome to Blackjack");
 
 console.log("You are dealt: ");
-console.log(" " + card1);
-console.log(" " + card2);
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
+
