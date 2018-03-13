dcfbecf918d0c640ee411031d6f8b6d4741fe58c
Cheniece Manning <cheniecemanning@hotmail.com>
2018-03-13 11:47:16 +0000

    Added UI  and integrated  JavaScript

--- blackjack.js
+++ blackjack.js
@@ -6,6 +6,20 @@ Author: Cheniece Manning
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
+let textArea = document.getElementById("text-area");
+let newGameBtn = document.getElementById("new-game-btn");
+let hitBtn = document.getElementById("hit-btn");
+let stayBtn = document.getElementById("stay-btn");
+
+hitBtn.style.display = 'none';
+stayBtn.style.display = 'none';
+
+newGameBtn.addEventListener('click', function(){
+textArea.innerText = 'Started...';
+newGameBtn.style.display = 'none';
+hitBtn.style.display = 'inline';
+stayBtn.style.display = 'inline';
+});
 function createDeck(){
     let deck = [];
     for (let suitIdx = 0; suitIdx<suits.length; suitIdx++){
