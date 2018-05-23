d757d87638ce37e50a93502285da2da8605bd4f2
Cheniece Manning <cheniecemanning@hotmail.com>
2018-05-23 14:44:49 +0100

    version 2 refactor

--- main.js
+++ main.js
@@ -1,62 +1,15 @@
-const audio = Array.from(document.querySelectorAll(".sound"));
-let yo = 0;
-
-document.addEventListener('keydown', play);
-document.addEventListener('keyup', bye);
-
-
-function play (e) {
+document.addEventListener('keydown', play = (e) => {
 
     document.getElementById(e.keyCode).classList.remove("key");
     document.getElementById(e.keyCode).classList.add("pretty");
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.play();
 
-        switch(e.keyCode){
-            case 65:
-             audio[0].play();
-            break;
-    
-            case 83:
+})
 
-             audio[1].play();
-            break;
-    
-            case 68:
-             audio[2].play();
-            break;
-    
-            case 70:
-            console.log("f");
-             audio[3].play();
-            break;
-    
-            case 71:
-             audio[4].play();
-            break;
-    
-            case 72:
-             audio[5].play();
-            break;
-    
-            case 74:
-             audio[6].play();
-            break;
-    
-            case 75:
-             audio[7].play();
-            break;
-    
-            case 76:
-             audio[8].play();
-            break;
-        }
-
-}
-
-function bye (e){
+document.addEventListener('keyup', bye = (e) => {
     //console.log(e.keyCode);
     document.getElementById(e.keyCode).classList.remove("pretty");
     document.getElementById(e.keyCode).classList.add("key");
 
-}
-
-
+})
