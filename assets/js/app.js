e074097e06719ee6e0ad57fffbb468b5f5a98e37
Cheniece Manning <cheniecemanning@hotmail.com>
2018-05-29 17:03:00 +0100

    Added review submit functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,74 @@
+(function () {
 
-(function(){
 
-  
 
     const app = angular.module("store", []);
-    app.controller('StoreController', function(){
+    app.controller('StoreController', function () {
         this.items = gem;
 
     });
-    app.controller('GalleryController', function(){
+    app.controller('GalleryController', function () {
         this.current = 0;
-        this.setCurrent = function(setCurrent) {
-           this.current = setCurrent || 0;
-          
-        };
-      });
+        this.setCurrent = function (setCurrent) {
+            this.current = setCurrent || 0;
 
-    app.controller ('PanelController', function (){
+        };
+    });
+
+    app.controller('PanelController', function () {
         this.tab = 1;
 
-      
-        this.isSet = function(checkTab){
+
+        this.isSet = function (checkTab) {
             return this.tab === checkTab;
         };
 
-        this.selectTab = function(setTab) {
+        this.selectTab = function (setTab) {
             this.tab = setTab;
         };
     });
 
-    let gem = [
-        {
-             image: [
-                'assets/imgs/gem-01.gif',
-                'assets/imgs/gem-02.gif'
-           ],
+    app.controller('ReviewController', function () {
+     this.review = {};
+     this.addReview = function(item) {
+         item.reviews.push(this.review);
+     };
+    });
+
+
+    let gem = [{
+
             name: 'Hexagon',
             price: 5.00,
             description: "Hexagonal shaped gemstone",
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            image: [
+                'assets/imgs/gem-01.gif',
+                'assets/imgs/gem-02.gif'
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this gem! Soooooo SPARKLY!!!",
+                    author: "shinyLover@gemworld.com"
+
+                },
+
+                {
+                    stars: 4,
+                    body: "I love this gem but TOO SPARKLY!!!",
+                    author: "kindaLikeShiny@gemworld.com"
+
+                },
+
+                {
+                    stars: 3,
+                    body: " It's ok but too expensive",
+                    author: "mrFrugal@gemworld.com"
+
+                }
+            ]
         },
 
         {
@@ -47,7 +76,8 @@
             price: 15.00,
             description: "Hexagonal shaped gemstone",
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            reviews: []
         },
 
         {
@@ -56,15 +86,16 @@
             description: "Hexagonal shaped gemstone",
             canPurchase: true,
             soldOut: true,
-            
+            reviews: []
         },
-        
+
         {
             name: 'Dodecahedron',
             price: 35.00,
             description: "Hexagonal shaped gemstone",
             canPurchase: true,
-            soldOut: true
+            soldOut: true,
+            reviews: []
         }
     ]
 })();
