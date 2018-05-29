524fb0d61046b7c53cd5acf917ca49117a8f1a8a
Cheniece Manning <cheniecemanning@hotmail.com>
2018-05-29 15:13:25 +0100

    Added gallery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,20 @@
 
 (function(){
+
+  
+
     const app = angular.module("store", []);
     app.controller('StoreController', function(){
         this.items = gem;
 
     });
+    app.controller('GalleryController', function(){
+        this.current = 0;
+        this.setCurrent = function(setCurrent) {
+           this.current = setCurrent || 0;
+          
+        };
+      });
 
     app.controller ('PanelController', function (){
         this.tab = 1;
@@ -22,7 +32,8 @@
     let gem = [
         {
              image: [
-                'assets/imgs/gem-01.gif'
+                'assets/imgs/gem-01.gif',
+                'assets/imgs/gem-02.gif'
            ],
             name: 'Hexagon',
             price: 5.00,
