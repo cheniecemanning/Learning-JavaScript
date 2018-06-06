8e6b37b5788ed2b8c1de052093a700239ce4857a
Cheniece Manning <cheniecemanning@hotmail.com>
2018-06-06 15:57:18 +0100

    Using props

--- comment-engine/src/App.js
+++ comment-engine/src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class CommentBox extends Component {
+class Comment extends Component {
   render() {
-    const topicsList = ['HTML', 'JavaScript', 'React'];
     const now = new Date();
     return (
-      <div> 
-        <h3>Story Box</h3>
-        <p className="lead">
-        Current time:{now.toLocaleTimeString()}
+      <div className="comment"> 
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">
+        <b>{this.props.body}</b>
+        <p className="comment-post-time">
+        Post time:{now.toLocaleTimeString()}
         </p>
+        </p>
+        <div className="commemt-footer">
+          <a href="#" className="comment-footer-delete">Delete Comment</a>
+        </div>
+       
 
-        <ul>
-          {topicsList.map(topic => <li>{topic}</li>)}
-        </ul>
+    
       </div> 
     );
   }
 }
 
-
+class CommentBox extends Component {
+  render() {
+    const topicsList = ['HTML', 'JavaScript', 'React'];
+    return ( 
+      <div className="comment-box">
+      <h3>Comments</h3>
+      <h4> Topics </h4>
+      <ul>
+          {topicsList.map(topic => <li>{topic}</li>)}
+        </ul>
+      <h5 className="comment-count">2 comments</h5>
+      <div className="comment-list">
+        <Comment author="Cheniece" body="This is amaze" />
+        <Comment author="Marcus" body="I hate this" />
+      </div>
+      </div>
+    );
+  }
+}
 export default CommentBox;
