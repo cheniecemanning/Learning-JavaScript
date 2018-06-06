637b7b5e40ebc5f806f5bf6d4ba04a1fbbdd5166
Cheniece Manning <cheniecemanning@hotmail.com>
2018-06-06 16:41:12 +0100

    Using method to create dynamic list

--- comment-engine/src/App.js
+++ comment-engine/src/App.js
@@ -27,27 +27,31 @@ class Comment extends Component {
 
 class CommentBox extends Component {
   render() {
-    const topicsList = ['HTML', 'JavaScript', 'React'];
-  
     //Underscore in method names helps distinguish custom methods from React Methods. 
-    const _getComments =  () => { //This Method will return an array of JSX elements
-
+    const _getComments = () => { //This Method will return an array of JSX elements
       const commentList =[
         {id: 1, author: "Cheniece", body: "This is amaze"},
         {id: 2, author: "Marcus", body: "This is horrible"},
-        {id: 3, author: "Joan", body: "This is Okay"}
+        {id: 3, author: "Joan", body: "This is Okay"},
+        {id: 4, author: "Harry", body: "This is Aight"}
       ];
-
-      //Each element from commentList is passed as an argument
-      return commentList.map((comment) => { // Returns an array
-         return (<Comment  author={comment.author}  body={comment.body}  />); //with a new component built for each elementmin the commentList array
-       });
+      
+    //Each element from commentList is passed as an argument
+    return commentList.map((comment) => { // Returns an array
+      //variables get passed into components as props
+      return (<Comment  author={comment.author}  body={comment.body} key={comment.id} />); //with a new component built for each elementmin the commentList array
+    });
     }
-
-    
+    const comments = _getComments();
     return ( 
-     _getComments()
-    );
+      <div className="comment-box">
+        <h3>Comments</h3>
+        <h4 className="comment-count">{comments.length} comments</h4>
+        <div className="comment-list">
+          {comments}
+        </div>
+      </div>
+     );    
   }
 }
 export default CommentBox;
