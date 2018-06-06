405e1a0fe281b2df4750296a8a3e83271f5de791
Cheniece Manning <cheniecemanning@hotmail.com>
2018-06-06 14:28:11 +0100

    Added a list and current time

--- story/src/App.js
+++ story/src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 
 class StoryBox extends Component {
   render() {
+    const topicsList = ['HTML', 'JavaScript', 'React'];
+    const now = new Date();
     return (
-      <div> <h1>Story Box</h1> </div> 
+      <div> 
+        <h3>Story Box</h3>
+        <p className="lead">
+        Current time:{now.toLocaleTimeString()}
+        </p>
+
+        <ul>
+          {topicsList.map(topic => <li>{topic}</li>)}
+        </ul>
+      </div> 
     );
   }
 }
