35a457b2e702d2a7865410ae11a493ac017e6fa4
Cheniece Manning <cheniecemanning@hotmail.com>
2018-08-21 12:46:53 +0100

    Added more ES6 learned stuff

--- es6_refresher/main.js
+++ es6_refresher/main.js
@@ -73,23 +73,78 @@ console.log(people2);
 
 const arr = [1, 2, 3];
 const arr2 = [...arr, 4];
-console.log(arr2);
+const arr3 = [...arr.filter(num => num !== 2)]
+console.log(arr3);
 
 const brad = {
     name: "Brad",
     age: 24
 };
 
-const person2 = {
-    ...brad,
-    email: "brad@gmail.com"
-};
+// const person2 = {
+//     ...brad,
+//     email: "brad@gmail.com"
+// };
+//console.log(person2)
 
-console.log(person2)
 // -----------------------DESTRUCTURING-----------------------
 
-// CLASSES
+const profile = {
+    name2: "Daphne",
+    address: {
+        street: "134 Cool Close",
+        city: "London"
+    },
+    hobbies: ["movies", "music"]
+};
+
+const {
+    name2,
+    address
+} = profile
+const {
+    street,
+    city
+} = profile.address
+console.log(`${name2} lives at ${street}`)
+
+// -----------------------CLASSES-----------------------
+class Person {
+    constructor(name) {
+        this.name = name;
+        console.log("ran");
+    }
+
+    greet() {
+        return `Hello my name is ${this.name}`
+    }
+}
+
+const aPerson = new Person("Sally");
+console.log(aPerson.greet());
 
 // SUBCLASSES
+//extends a Class
+
+class Customer extends Person {
+    constructor(name, age, balance) {
+        super(name);
+        this.age = age;
+        this.balance = balance;
+    }
+
+    info() {
+        return `${this.name} owes £${this.balance}.00 and is ${this.age} years old`
+    }
+}
+
+const customer1 = new Customer('Kevin', 32, 500);
+console.log(customer1.info())
 
 // MODULES
+
+//file 1 (file.js)
+//export const theName = "Jeff";
+
+//file 2 (file2.js)
+//import { theName } from './file'; 
