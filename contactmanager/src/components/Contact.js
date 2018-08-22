0620b354d2c750837e8bfeab99908e24680e48f5
Cheniece Manning <cheniecemanning@hotmail.com>
2018-08-22 16:21:53 +0100

    Added onDeleteClick, remove contacts on click

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -3,25 +3,49 @@ import PropTypes from 'prop-types';
 
  class Contact extends Component {
    
-    onShowClick = () => {
-      console.log("hiii")
-    }
+  state = {
+    showContactInfo: true
+  }
+    
+  onDeleteClick = () => {
+    this.props.deleteClickHandler()
+  }
+
   render() {
     const { contact } = this.props
+    const { showContactInfo } = this.state
+
     return (
       <div className='card card-body mb-3'>
-        <h2>{contact.name} <i onClick={this.onShowClick} className='fas fa-sort-down'/></h2>
-        <ul className='list-group'>
-          <li className='list-group-item'>Email: {contact.email}</li>
-          <li className='list-group-item'>Phone number: {contact.phone}</li>
-        </ul>
+        <h2>{contact.name} {' '}
+           <i 
+            onClick={() => this.setState({showContactInfo: !this.state.showContactInfo})} 
+            className='fas fa-sort-down'
+            style= {{cursor: 'pointer'}}
+          />
+
+           <i 
+            onClick={this.onDeleteClick} 
+            className='fas fa-times'
+            style= {{cursor: 'pointer', float: 'right', color: 'crimson'}}
+          />
+        </h2>
+        { showContactInfo ? (   
+          <ul className='list-group'>
+            <li className='list-group-item'>Email: {contact.email}</li>
+            <li className='list-group-item'>Phone number: {contact.phone}</li>
+          </ul>
+          ) : null
+        }
+     
       </div>
     )
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 }
 
 export default Contact;
--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -13,18 +13,28 @@ class Contacts extends Component {
             ]
     }
     
+    deleteContact = id => {
+        const { contacts } = this.state
+        const newContacts = contacts.filter(contact => contact.id !== id)
+        console.log()
+        this.setState({
+            contacts: newContacts
+        })
+    }
+
     render() {
      const { contacts } = this.state;
 
     return (
-      <div>
+      <React.Fragment>
            {contacts.map(contact => (
                <Contact
                     key = {contact.id}
                     contact = {contact}
+                    deleteClickHandler={this.deleteContact.bind(this, contact.id)}
                 />
            ))} 
-      </div>
+      </React.Fragment>
     )
   }
 }
