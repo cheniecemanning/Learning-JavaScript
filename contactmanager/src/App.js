5b040002d6756d0f988fe430770dda0777d9eb79
Cheniece Manning <cheniecemanning@hotmail.com>
2018-08-23 10:40:09 +0100

    Added addContact form

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Contacts from './components/contacts/Contacts'
 import Header from './components/layout/Header'
-
+import AddContacts from './components/contacts/AddContact'
 import { Provider } from './context'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -14,6 +14,7 @@ class App extends Component {
         <div className="App">
           <Header brand="Contact Manager" />
         <div className='container'>
+          <AddContacts />
           <Contacts />
         </div>
       </div>
---
+++ contactmanager/src/components/contacts/AddContact.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+class AddContact extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone: ''
+    }
+
+    onChange = (e) => this.setState({[e.target.name]: e.target.value})
+    
+    render() {
+      const { name, email, phone } = this.state
+    return (
+      <div className="card mb-3 col-6">
+        <div className="card-header">
+            Add Contact
+        </div>
+        <div className="card-body">
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        placeholder="Enter Name"
+                        className="form-control form-control-lg" 
+                        value={name}
+                        onChange={this.onChange}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Enter Email"
+                        className="form-control form-control-lg" 
+                        value={email}
+                        onChange={this.onChange}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="phone">Phone</label>
+                    <input 
+                        type="text"
+                        name="phone"
+                        placeholder="Enter Phone Number"
+                        className="form-control form-control-lg" 
+                        value={phone}
+                        onChange={this.onChange}
+                    />
+                </div>
+                <input 
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-light btn-block"
+                />
+            </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddContact
